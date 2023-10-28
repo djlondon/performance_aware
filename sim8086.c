221618258ee23685ca0c94526b66b87202163a00
Dominic London <25865081+djlondon@users.noreply.github.com>
2023-10-28 17:37:24 +0100

    single line

--- sim8086.c
+++ sim8086.c
@@ -14,7 +14,7 @@ const int MAX_BYTES = 512;
 
 int read_file(const char *fname);
 
-void parse_byte1(uint8_t byte, uint8_t *W, uint8_t *D)
+void parse_byte1(uint8_t byte, uint8_t *W, uint8_t *D, char *out)
 {
     // first 6 bytes determine operand
     uint8_t opcode = byte >> 2;
@@ -22,56 +22,55 @@ void parse_byte1(uint8_t byte, uint8_t *W, uint8_t *D)
     *D = (byte & 0x02) >> 1;
     // last byte
     *W = (byte & 0x01);
-    char *op = "";
     if (opcode == 0b100010)
     {
-        op = "mov";
+        sprintf(out, "mov");
     }
-    printf("%x: %s %d %d\n", byte, op, *D, *W);
+    // printf("%x: %s %d %d\n", byte, op, *D, *W);
 }
 
 void parse_reg(uint8_t byte, uint8_t W, char out[3])
 {
     if (byte == 0 || (byte == 4 && W == 0))
-        out[0] = 'A';
+        out[0] = 'a';
     else if (byte == 1 || (byte == 5 && W == 0))
-        out[0] = 'C';
+        out[0] = 'c';
     else if (byte == 2 || (byte == 6 && W == 0))
-        out[0] = 'D';
+        out[0] = 'd';
     else if (byte == 3 || (byte == 7 && W == 0))
-        out[0] = 'B';
+        out[0] = 'b';
     else if (W == 1)
     {
         if (byte == 4)
-            out[0] = 'S';
+            out[0] = 's';
         if (byte == 5)
-            out[0] = 'B';
+            out[0] = 'b';
         if (byte == 6)
-            out[0] = 'S';
+            out[0] = 's';
         if (byte == 7)
-            out[0] = 'D';
+            out[0] = 'd';
     }
     /* out[1]*/
     if (W == 0)
     {
         if (byte < 4)
-            out[1] = 'L';
+            out[1] = 'l';
         else
-            out[1] = 'H';
+            out[1] = 'h';
     }
     if (W == 1)
     {
         if (byte < 4)
-            out[1] = 'X';
+            out[1] = 'x';
         else if (byte < 6)
-            out[1] = 'P';
+            out[1] = 'p';
         else
-            out[1] = 'I';
+            out[1] = 'i';
     }
     out[2] = '\0';
 }
 
-void parse_byte2(u_int8_t byte, uint8_t W, uint8_t D)
+void parse_byte2(u_int8_t byte, uint8_t W, uint8_t D, char out[])
 {
     // ABXXXXXX: first 2 bytes
     uint8_t MOD = byte >> 6;
@@ -92,10 +91,10 @@ void parse_byte2(u_int8_t byte, uint8_t W, uint8_t D)
     }
     // if D regout, rmout else rmout, regout
     if (D)
-        printf("%s, %s\n", regout, rmout);
+        sprintf(out, "%s, %s\n", regout, rmout);
     else
-        printf("%s, %s\n", rmout, regout);
-    printf("%d: %d %d %s %d %s\n", byte, MOD, REG, regout, RM, rmout);
+        sprintf(out, "%s, %s\n", rmout, regout);
+    // printf("%d: %d %d %s %d %s\n", byte, MOD, REG, regout, RM, rmout);
 }
 
 int main(int argc, char *argv[])
@@ -125,6 +124,9 @@ int read_file(const char *fname)
 
     int c, i = 0; // note: int, not char, required to handle EOF
     uint8_t W, D = 0;
+    char opout[4];
+    char rout[8];
+    char lineout[14];
     while ((c = fgetc(fp)) != EOF)
     {
         if (c == '\n')
@@ -135,10 +137,12 @@ int read_file(const char *fname)
         switch (i)
         {
         case 0:
-            parse_byte1(c, &W, &D);
+            parse_byte1(c, &W, &D, opout);
             break;
         case 1:
-            parse_byte2(c, W, D);
+            parse_byte2(c, W, D, rout);
+            sprintf(lineout, "%s %s\n", opout, rout);
+            puts(lineout);
             break;
         }
         i++;
@@ -147,7 +151,6 @@ int read_file(const char *fname)
         puts("I/O error when reading");
     else if (feof(fp))
     {
-        puts("End of file is reached successfully");
         is_ok = EXIT_SUCCESS;
     }
 
